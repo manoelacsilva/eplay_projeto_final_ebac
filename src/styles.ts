6f56f2babbee634c4fe52e3b4c3d46e0364bda1e
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-12-08 22:18:14 -0300

    commit inicial: fazendo o header

---
+++ src/styles.ts
@@ -0,0 +1,30 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const cores = {
+  branca: '#eee',
+  preta: '#111',
+  cinza: '#333',
+  verde: '#10ac84'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+  }
+
+  body {
+    background-color: ${cores.preta};
+    color: ${cores.branca};
+    padding-top: 40px;
+  }
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+`
