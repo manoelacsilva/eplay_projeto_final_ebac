af1b2dea6722e452ca3561d6dafeb6f0389a6a5f
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-12-10 22:35:17 -0300

    criando o banner e componentize parte 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Banner from './components/Banner'
 import Header from './components/Header'
-import { GlobalCss, Container } from './styles'
+import { GlobalCss } from './styles'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Banner />
+  }
+])
 
 function App() {
   return (
     <>
       <GlobalCss />
-      <Container>
+      <div className="container">
         <Header />
-      </Container>
+      </div>
+      <RouterProvider router={rotas} />
     </>
   )
 }
--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 export const cores = {
   branca: '#eee',
@@ -21,10 +21,10 @@ export const GlobalCss = createGlobalStyle`
     color: ${cores.branca};
     padding-top: 40px;
   }
-`
 
-export const Container = styled.div`
-  max-width: 1024px;
-  width: 100%;
-  margin: 0 auto;
+  .container {
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+  }
 `
