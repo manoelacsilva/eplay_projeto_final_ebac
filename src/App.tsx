c406209de3da345424608f829a3595b28702cd8c
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-12-12 00:19:18 -0300

    componentize partes 2 e 3 e construindo páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import Banner from './components/Banner'
 import Header from './components/Header'
 import { GlobalCss } from './styles'
+import Home from './pages/Home'
 
 const rotas = createBrowserRouter([
   {
     path: '/',
-    element: <Banner />
+    element: <Home />
   }
 ])
 
